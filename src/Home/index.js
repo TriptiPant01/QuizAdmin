ddedad9abeaf42c8791c7371bb024186a4a14fef
Tripti Pant <itsmetripti20@gmail.com>
2018-07-25 21:07:06 +0545

    initial commit

---
+++ src/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+	render () {
+		return (
+			<div style ={{flex:1, justifyContent:'center', alignItems:'center'}}>
+				Welcome
+			</div>
+		)
+	}
+}
+
+export default Home
